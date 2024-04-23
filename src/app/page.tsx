04c5e318cbeca063b467c3cf8b94be774f502eef
Darkness-2 <justinmmm690@gmail.com>
2024-04-23 18:02:28 -0400

    Force dynamic on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return (
     <main className="">
