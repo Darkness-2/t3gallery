4864e9c9d8f32d8d0cc51d6139ef8e46efd22f57
Darkness-2 <justinmmm690@gmail.com>
2024-04-27 20:58:57 -0400

    Format modal

--- src/components/FullImagePage.tsx
+++ src/components/FullImagePage.tsx
@@ -7,5 +7,14 @@ type Props = {
 export default async function FullImagePage({ id }: Props) {
   const image = await getImage(id);
 
-  return <img src={image.url} className="w-96" />;
+  return (
+    <div className="flex h-full w-full text-white">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l border-white">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -25,7 +25,7 @@ export function Modal({ children }: { children: ReactNode }) {
       onClose={onDismiss}
     >
       {children}
-      <button onClick={onDismiss} className="close-button" />
+      {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
     document.getElementById("modal-root")!,
   );
